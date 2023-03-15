c10a4570b3ba025ea5f2f9bea65d552a2cd23a57
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-03-15 14:23:15 +0500

    1_dars
    Python

---
+++ 1 dars.py
@@ -0,0 +1,16 @@
+# print('assalomu alaykum')
+
+
+# print('xush kelibciz')
+# ism=input('ismingizi krting: ')
+# print('salom', ism)
+# a=int(input('a sonni kirtin '))
+# b=int(input('b sonni kirtin '))
+# print('a+b=',a+b)
+# son1 = 10
+# son2 = 33  
+# print('siznig raqamingiz', son1)
+# print('son2',son2)
+name=input('ismingizi kiriting:')
+age=int(input('yushingizi kirting:'))     
+print('assalomu alaykum', name ,'siznig yoshingiz', age,'da')        
