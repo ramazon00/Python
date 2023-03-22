2de39acf014cb78bd3ec97a344c97d598bb4f7df
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-03-22 06:51:18 +0500

    7_dars
    Python

---
+++ 7-dars.py
@@ -0,0 +1,98 @@
+# a = 1
+# mehmonlar = ['Ali','Vali','Hasan','Husan','Jamshid']
+# for mehmon in mehmonlar:
+#     print(f"""{a}.Assalomu alaykum qadirli do'stim {mehmon.capitalize()},
+# sizni 20 Dekabr kuni SHERBEK OTA to'yxonasiga nahorgi oshga taklif etamiz""")
+#     print("Hurmat bilan, murodjon oilasi\n")
+#     a +=1
+
+# sonlar = [1, 2, 3, 4, 5,]
+# for a in sonlar:
+#     print(a, end=" ")
+
+# sonlar = list(range(1,11))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+#     print(f"{son} ning kubi {son**3} ga teng\n")
+
+
+# a_1 = [1, 3, 5]
+# b_1 = [2, 4, 6, 9, 10, 11]
+# for a in a_1:
+#     for b_1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
