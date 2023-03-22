372478630f3b033be1483d0c0378f12a86046cb4
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-03-22 06:52:37 +0500

    8_dars
    Python

---
+++ 8-dars.py
@@ -0,0 +1,97 @@
+# import avto_dars_mod
+# avto1=avto_dars_mod.avto_info("GM","Malibu","oq","avtomat",2022,40000)
+# avto_dars_mod.info_print(avto1)
+
+# import avto_dars_mod as a
+# avto1=a.avto_info("GM","Malibu","oq","avtomat",2022,40000)
+# a.info_print(avto1)
+
+
+# import avto_dars_mod as a
+
+# avto1=a.avto_kirit()
+# for avto in avto1:
+#     a.info_print(avto)
+
+
+
+# from avto_dars_mod import avto_info, info_print
+
+# avto1=avto_info("GM","Malibu","oq","avtomat",2022,40000)
+# info_print(avto1)
+
+# from avto_dars_mod import avto_info as ainfo, info_print as iprint
+# avto1=ainfo("GM","Malibu","kulrang","avtomat",2022,40000)
+# iprint(avto1)
+
+
+# from avto_dars_mod import*
+# avto1=avto_info("GM","Malibu","qora","avtomat",2022,40000)
+# info_print(avto1)
+
+
+# avtolar = ['audi','bmw','volvo','kia','hyundai','bmw']
+# for k in avtolar:
+#     if k == 'bmw':
+#         print(k.upper())
+#     else:
+# #         print(k.title())
+
+# javob = float(input("12x6 nechiga teng>>>"))
+# if javob != 72:
+#     print("afsuski sizning Javobingiz hato!")
+# else:
+#     print("tabriklaymiz sizning Javobingiz to'g'ri")
+
+
+# yosh = int(input("Yoshingiz nechida?>>>"))
+# if yosh>=18:
+#     print("Xush kelibsiz")
+# else:
+#     print("Kirish mumkin emas")
+
+
+# login = input("Yangi Login tanlang: ")
+# if len(login)<=5:
+#     print("login 5ta harfdan ko'proq bo'lishi shart!")
+# else:
+#     print("Login qabul qilindi!")
+
+# yil = int(input("Tug'ilgan yilingizni kiriting: "))
+# if 2022-yil<18:
+#     print(f"Yoshingiz {2022-yil}da ekan")
+#     print("Kirish mumkin emas!")
+# else:
+#     print(f"Yoshingiz {2022-yil}da ekan")
+#     print("Xush kelibsiz")
+
+
+# yosh = int(input("Yoshingiz nechida?>>> "))
+# if yosh > 65:
+   
+
+
+#     print("Siz COVID-19 ga chalinishga moil ekansiz")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
