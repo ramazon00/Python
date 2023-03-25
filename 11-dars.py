8743a5742ae2bfd4b6b3400c1df05e3b154b562d
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-03-25 11:10:28 +0500

    11_dars
    Python

---
+++ 11-dars.py
@@ -0,0 +1,252 @@
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sat Nov 26 12:01:09 2022
+
+# @author: IT CENTER YANGIYER
+# """
+
+# # son = int(input)("Bugun necha odam bilan ko'rishdingiz?")
+# # ismlar = []
+# # for i in range(1, son +1):
+# #     ism = input(f"i+1")
+
+# # yosh = int(input('Yoshingiz nechida? '))
+# # if yosh <= 4:
+# #     print('Sizga kirish bepul.')
+# # elif yosh <= 12:
+# #     print("Sizga kirish 5000 so'm")
+# # elif yosh <= 18:
+# #     print('Sizga kirish 8000 som')
+# # else:
+# #     print('Sizga kirish 10000 som')
+
+# # yosh = int(input('Yoshingiz nechida? '))
+# # if yosh <= 4:
+# #     price = 0 
+# # elif yosh <= 12:
+# #     prise = 5000
+# # elif yosh <= 18:
+# #     prise = 8000
+# # else:
+# #     prise = 10000
+# # print(f"Sizga kirish {prise} som12")
+
+
+# # kun = input("Bugun kun nima?>>> ")
+# # if kun.lower() == 'shanba' or kun.lower() == 'Yakshanba':
+# #     print('Bugun dam olish kuni')
+# # else:
+#    # print('Bugun ish kuni.')
+  
+# # kunlar = ['dushanba', 'seshanba','chorshanba','payshanba','juma','shanba','yakshanba']
+# # kun = input("Bugun nima kun? ")
+# # harorat = float(input("Havo harorati qanday? "))
+# # for i in kunlar:
+# #     if kun.lower()=='yakshanba' and harorat>=30:
+# #       print("chomilgani kettik")
+# #     elif kun.lower()=='yakshanba' and  harorat<30:
+# #       print("uyda dam olamiz!")
+# #     elif kun.lower()=='yakshanba' and  harorat<30:
+  
+    
+# # kun = input("bugun nima kun?>>> ")
+# # if kun.lower() == 'c' or kun.lower() == 'yakshanba':
+# #     print('bugun dam olish kuni. ')
+# # else:   
+# #     print('bugun ish kuni. ')
+
+
+# # choy = True
+# # salat = True
+# # prise = 15000
+# # if choy and salat:
+# #     prise = prise + 10000
+# # elif choy or salat:
+# #     prise = prise + 5000
+# # print(f"Jami {prise} som")
+
+# # narh = 1500
+# # choy = True
+# # salat = True
+# # kompot = True
+# # assorti = True
+
+# # if choy:
+# #     print("Jamshid choy oldi")
+# #     narh +=3000
+# # if salat:
+# #     print("Jamshid salat oldi")
+# #     narh +=5000
+# # if assorti:
+# #     print("Jamshid assorti oldi")
+# #     narh +=10000
+# #     print(f"jami {narh} som")
+
+
+# # ism = input("talaba ism va familyasini kiriting: ")
+# # baho = int(input(f"{ism.title()}ni baholang>>> "))
+# # if baho ==5:
+# #     print("alo")
+# # elif baho ==4:
+# #     print("yaxshi")
+# # elif baho <= 3:
+# #     print("yomon")
+# # else:
+# #     print("istimos 1 dan 6 gacha son kirgazing")
+
+
+# # menu = ("osh","kabob","manti","norin","mastava")
+# # ovqat = input ("Nima ovqat yeysiz? ")
+# # if ovqat.lower() not in menu:
+# #     print("Afsuski bizda bunday ovqat yo'q")
+# # else:
+# #     print("buyutma qabul qilindi")
+
+
+# # menu = []
+# # for son in range(1, 6):
+# #     menu.append(input(f"{son}-taom kiriting: "))
+# # taom = input("qanday taom buyutma qilasiz?>>> ")
+# # if taom not in menu:
+# #     print(f"kechirasiz bizda {taom} yoq! bizdagi taomlar ro'yxati: ")
+# #     for a in menu:
+# #         print(a, end=("\n"))
+# #     taom_2 = input("iltimos quyidagi taomlardan buyutma qiling ")
+# #     print(f"{taom_2} qabul qilindi!")
+# # else:
+# #     print(f"{taom} qabul qilindi!")
+
+# # menu = ['osh','qozonkabob','shashlik','norin','somsa']
+# # buyutmalar = ["osh","somsa","manti","shashlik"]
+# # if buyutmalar:
+# #     for taom in  buyutmalar:
+# #         if taom in menu:
+# #             print(f"Menuda {taom} bor")
+# #         else:
+# #             print(f"Kechirasiz, menuda {taom} yo'q")
+# # else:
+# #     print("Savatchangiz bo'sh!")
+#     # 
+
+# # lugat = {"salom": "hello", "kitob": "book"}
+# # print(f"salomning inglischa tarjimasi - {lugat['salom']}")
+# # print(f"kitobning inglischa tarjimasi - {lugat['kitob']}")
+    
+    
+# talaba_0 = {    
+#     'ism':'Murod Ibragimov',
+#     'yosh':22,
+#     't_yil':2005
+# }
+# # print(f"""{talaba_0['ism'].title()}),
+# # {talaba_0['t_yil']}-yilda tug'ilgan,
+# # {talaba_0['yosh']} yoshda""")
+
+# # talaba_0['kurs'] = 4
+# # talaba_0['fakultet'] = 'informatika'
+# # talaba_0['ism'] = 'Abdulloh'
+
+# # print(talaba_0)
+# # print(f"""{talaba_0['ism'].title()}, \
+# # {talaba_0['t_yil']} -yilda Sirdaryo viloyatida tug'ilgan, \ 
+# # {talaba_0['yosh']} yoshda, \
+# # {talaba_0['fakultet']} fakultetining, \
+# # {talaba_0['kurs']} kursda o'qiydi""")
+# # print('Hozirda yotoqxonada yashamoqda' )
+
+# # talaba_1 = {}
+# # talaba_1['ism'] = 'qobil rasulov'
+# # talaba_1['kurs'] = 3
+# # talaba_1['yosh'] = 20
+# # print(talaba_1)
+# # print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}")
+# # talaba_1['kurs'] = 4
+# # print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}")
+
+# print(talaba_0)
+# del talaba_0['yosh']
+# del talaba_0['t_yil']
+# print(talaba_0)
+
+# telefonlar = {
+#     'ali':'iphone',
+#     'vali': 'galaxy s9',
+#     'olim':'nokia 3310'
+#     }
+# phone = telefonlar['ali']
+# print(f"alining telefoni {phone}")
+# phone = telefonlar['vali']
+# print(f"valining telefoni {phone}")
+# phone = telefonlar['hasan']
+# print(f"hasanning telefoni {phone}")
+
+# phone = telefonlar.get('ali','Bunday ism mavjud emas')
+# print(phone)
+# phone = telefonlar.get('hasan',"hasan disk yo'q")
+# print(phone)
+
+# talaba = {}
+# key = input("ism kiriting>>> ").lower()
+# value = input(f"{key}ga valueni kiriting>>>  ").lower()
+# talaba[key] = value
+# print(talaba)
+
+# talaba_0 = {
+#     'ism': 'Alijon',
+#     'familya': 'Shamsiyev',
+#     'yosh': 22,    
+#     'fakultet': 'Informatika',
+#     'kurs': 4
+#     }
+# key = input("Kalit sozni kiriting: ")
+# if key.lower() in talaba_0:
+#     print(talaba_0[key])
+# else:
+#     key = input("bunday kalit soz mavjud emas iltimos kalit soz va qiymat kiriting.\nKey>>> ")
+#     value = input("value>>> ")
+#     talaba_0[key] = value
+#     print(f"{key} -- {talaba_0[key]}") 
+ 
+# print(talaba_0.items())   
+    
+# for kalit, qiymat in talaba_0.items():
+#     print(f"Kalit: {kalit}")
+#     print(f"Qiymat: {qiymat} \n")
+    
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxu s9',
+#     'olim':'mi 10 pro',
+#     'orif':'nokia 3310',
+#     'ozim':'iphone 14 pro mac'
+#     }
+# for key, value in telefonlar.items():
+#     print(f"{key.title()}ning telefoni {value}")    
+    
+# mahsulotlar = {  
+#     'olma':10000,
+#     'anor':20000,
+#     'uzum':40000,
+#     'anjir':25000,
+#     'shaftoli':30000
+#     }
+# # print('dokondagi mahsulotlar: ')
+# # for mahsulot in mahsulotlar.keys():
+# #     print(mahsulot.title())
+    
+# bozorlik = ['anor','uzum','non','baliq']   
+# for mahsulot in mahsulotlar:
+#     if mahsulot in bozorlik:
+#         print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
+# for buyum in bozorlik:
+#     if buyum not in mahsulotlar:
+#         print(f"iltimos, dokoningizga {buyum} ham olib keling")
+    
+    
+    
+    
+    
+    
+    
+    
+    
