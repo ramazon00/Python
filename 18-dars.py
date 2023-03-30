541e3fbd99e1e13d434566331d7ab83116e9b49f
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-03-30 16:08:48 +0500

    18_dars
    Python

---
+++ 18-dars.py
@@ -0,0 +1,59 @@
+# # -*- coding: utf-8 -*-
+
+
+# def talabani_bahola(ismlar):
+#     baholar = {}
+#     while ismlar:
+#         ism = ismlar.pop()
+#         baho = input(f"Talaba {ism.title()}ning bahosini kiriting: ")
+#         baholar[ism] = int(baho)
+#     return baholar
+
+# baho = talabani_bahola(["ali", "vali", "hasan", "husan"])
+# print(baho)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
