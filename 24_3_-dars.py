6f29f282e3db9788d4810c55db71ef6961ed5f2b
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-04-04 15:01:01 +0500

    24_dars
    3_qismi

---
+++ 24_3_-dars.py
@@ -0,0 +1,32 @@
+# class Talaba:
+#     """Talaba klassi"""
+
+#     def __init__(self, ism, passport, tyil,idraqam,manzil):
+#         super().__init__(ism, passport, tyil)
+#         self.idraqam=idraqam
+#         self.bosqich = 1
+#         self.manzil=manzil
+#         self.ism=ism
+#         self.passport=passport
+#         self.tyil=tyil
+    
+
+    
+# class Fan(Talaba):
+#     def __init__(self, ism, passport, tyil,idraqam,manzil,nomi,baho,chorak):    
+#         super(). __init__(self, ism, passport, tyil,idraqam,manzil)
+#         def set_baho(self,baho):
+#             self.baho=baho
+#         def get_baho(self):
+#             return self.baho
+#         def get_age(self,yil):
+#             "Talaba yoshini qaytuvchi metod"
+#             return yil - self.tyil
+#         def get_id(self):        
+#             return self.idraqam
+#         def get_manzil
+
+
+
+
+
